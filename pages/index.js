fded03cd0e5b78f422ae80e0ef6804b42479f63a
abdelhammied-elsayed <91680646+abdelhammied-elsayed@users.noreply.github.com>
2022-05-24 14:35:33 +0200

    feat(add head supporting): head Supporting for seo

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,30 @@ import React from "react";
 import Layout from "../layouts/layout";
 import LatestNews from "../components/latest-news/latest-news";
 import Slider from "../components/slider/slider";
+import Head from "next/head";
 
-export default function Index() {
+export default function Index(props) {
   return (
-    <Layout>
-      <LatestNews latestNews={"Latest News From Backend"} />
+    <>
+      <Head>
+        <meta name="author" content={props.name} />
 
-      <Slider />
-    </Layout>
+        <title>We are here</title>
+      </Head>
+
+      <Layout>
+        <LatestNews latestNews={props.name} />
+
+        <Slider />
+      </Layout>
+    </>
   );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      name: "ibtikar technologies",
+    },
+  };
 }
